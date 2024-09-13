a73d07c46d46c0743a149b565e893a117fb3f826
Harshil089 <74847981+Harshil089@users.noreply.github.com>
2024-09-13 09:51:07 +0530

    Create modify.py

---
+++ Numpy/modify.py
@@ -0,0 +1,24 @@
+import  numpy as np
+# a = np.array([[1, 2, 3,4,5],[12,435,758,536,98],[87,90,56,87,22]])
+# print(a)
+# print(a[0,3],a[1,3],a[2,3])
+# print(a[0,:])
+# print(a[:,3])
+# print(a[0,0:5:2])
+# a=np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+a=np.array([1,2,3,4])
+
+print(a)
+# print(a[0,1,1])
+# print(a[:,1,:])
+# a[:,1,:]=[[9,9],[10,10]]
+# print(a)
+b=np.array([1,2,3,4])
+print(b)
+print(b-2)
+print(b*2)
+print(b/2)
+print(b+2)
+print(b**2)
+print(np.sin(b))#cos,tan,etc
+print(a+b)
