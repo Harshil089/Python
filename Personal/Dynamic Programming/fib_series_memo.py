70c3f397c82a22b87538396bb8167321f0fe638d
Harshil089 <74847981+Harshil089@users.noreply.github.com>
2024-10-10 14:00:27 +0530

    Commit fib_series_memo.py

--- Personal/Dynamic Programming/fib_series_memo.py
+++ Personal/Dynamic Programming/fib_series_memo.py
@@ -1,18 +1,18 @@
-def fib (n,memo={}):
+def fibonacci(n, memo):
     if n in memo:
         return memo[n]
-    if n<=2:
-        return n
-    memo[n]=fib(n-1,memo) + fib(n-2,memo)
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
     return memo[n]
-n=199
-print(f"The Fibonacci series for n={n} is: {fib(n)}")
 
-# def fibonacci(n):
-#     if n == 0:
-#         return 0
-#     if n == 1:
-#         return 1
-#     return fibonacci(n - 1) + fibonacci(n - 2)
-# n = 50
-# print(f"Fibonacci number at position {n} is: {fibonacci(n)}")
+def printFibonacciSeries(n):
+    memo = {}
+    for i in range(n):
+        print(fibonacci(i, memo), end=" ")
+
+n = int(input("Enter the number of terms in Fibonacci series: "))
+print(f"Fibonacci series up to {n} terms:")
+printFibonacciSeries(n)
